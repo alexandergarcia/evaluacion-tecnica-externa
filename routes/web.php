f0e15577e935b636994c71c001bbf3c71222cb6e
Alexander Garcia <alexandergarcia20@outlook.es>
2024-10-15 13:54:12 -0500

    latest changes

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TaskController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('home');
 });
 Auth::routes();
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
